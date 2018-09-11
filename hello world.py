2c18240fb4a66c4b92ab0c514e6d3f0c17310df8
Kyn <kyn@Kyns-MacBook-Pro.local>
2018-09-11 11:13:03 -0400

    finished first homework

---
+++ hello world.py
@@ -0,0 +1,30 @@
+x = 2
+
+print ("Hello!")
+username = input("What is your name?")
+print ("Nice to meet you, "+username+"!")
+date = input("sorry "+username+", but what day of the week is it again?")
+print (""+date+" already?!")
+sport = input("what sport are you doing after school?")
+print (""+sport+" is a very fun sport!")
+classes = int(input("Now, how many classes do you have today again pssst*answer with a number please*?"))
+if classes > x:
+	print('WOW,thats alot you hard worker')
+else:
+	print("lucky!")
+videogame = input("what is your favorite video game, just pick one no matter what.")
+print ("i agree, "+username+"! "+videogame+" is a great game,  i love it!")
+free = input("by the way "+username+", what do you like to do in your freetime?")
+print ("No way! my hobby is "+free+" too!!")
+number = input("hey, "+username+" i've had a great time speaking to you, what is your number?")
+print ("hmmmmm "+number+" ,thats a pretty cool number")
+joke = input("knock knock")
+boo = input("boo")
+print ("why are you crying?")
+rate = int(input("how was my joke on a scale of 1 to 5? one being horrible and five being AMAZING!"))
+if rate < x:
+		print('*sobs* I worked hard on that joke, goodbye')
+else:
+	print ("Im glad you liked it!")
+	time = input("sorry "+username+", but what time is it?")
+	print("I have to go but it's been a great conversation and i had alot of fun, thanks for spending time with me "+username+".")
